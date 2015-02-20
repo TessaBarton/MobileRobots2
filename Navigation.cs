68afadae25c45023653b9d0930379aaa02c9e889
Tessa Barton <tbarton16@gmail.com>
2015-02-20 10:41:34 -0800

    changed rotational velocity functions

--- Navigation.cs
+++ Navigation.cs
@@ -504,16 +504,14 @@ namespace DrRobot.JaguarControl
 
             double goalX = desiredX -x;
             double goalY = desiredY-y;
-            double dTheta = t-desiredT;// QUESTION!
-            Console.WriteLine("X,Y (" + x.ToString() + " , " + y.ToString() + " )");
-            Console.WriteLine("desired X,Y (" + desiredX.ToString() + " , " + desiredY.ToString() + " )");
+            double dTheta = desiredT - t;// QUESTION!
 
             double desiredV, desiredW,pho,alpha,beta;
             if (goalX < 0)
             { //we are headed in the backward direction
                 pho = Math.Sqrt(Math.Pow(goalX, 2.0) + Math.Pow(goalY, 2.0));// distance from curLoc to desLoc
                 alpha = -t + Math.Atan2(-goalY, -goalX);//
-                beta = -t - alpha;
+                beta = -t - alpha +dTheta;
 
                 alpha = newNormalizeAngle(alpha);
                 beta = newNormalizeAngle(beta);
@@ -526,20 +524,21 @@ namespace DrRobot.JaguarControl
                 //transform coordinate systems
                 pho = Math.Sqrt(Math.Pow(goalX, 2.0) + Math.Pow(goalY, 2.0)); //pho is linear distance
                 alpha = -t + Math.Atan2(goalY, goalX); // alpha is angle between robot facing and destination
-                beta = -t - alpha; //angle between pho idk
+                beta = -t - alpha +dTheta; //angle between pho idk
                 alpha = newNormalizeAngle(alpha);
                 beta = newNormalizeAngle(beta);
                 desiredV = Kpho * pho;
                 desiredW = (Kalpha * alpha) + (Kbeta * beta); //correct
 
             }
-            Console.WriteLine("desired pho, alpha,beta (" + pho.ToString() + " , " + alpha.ToString() + " , " + beta.ToString() + " )");
-            Console.WriteLine("desiredV, desiredW (" + desiredV.ToString() + " , " + desiredW.ToString() + " )");
+
+            double L = 2 * robotRadius;
             //calculate rotational velocities, convert to wheel rotation rates
-            double rotVelocity1 = desiredW - (((desiredV / robotRadius) - desiredW) / 2);
-            double rotVelocity2 = ((desiredV / robotRadius) - desiredW) / 2;
-            desiredRotRateR = (short)(2 * robotRadius * rotVelocity1 / wheelRadius);
-            desiredRotRateL = (short)(-2 * robotRadius * rotVelocity2 / wheelRadius);
+            double rotVelocity1 = (desiredW/2)+(desiredV /(2*robotRadius));
+            double rotVelocity2 = (desiredW /2) -(desiredV /( 2 * robotRadius));
+
+            desiredRotRateR = (short)((2 * robotRadius * rotVelocity1) / wheelRadius);
+            desiredRotRateL = (short)((-2 * robotRadius * rotVelocity2) / wheelRadius);
             // convert to encoder pulses per second
 
             desiredRotRateR = (short)(desiredRotRateR * (1 / (2 * Math.PI * wheelRadius)) * 190);
